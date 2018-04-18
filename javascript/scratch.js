af8540e95740ef6e8f0941f3ddef169cb6ab0bce
Peter Coles <peter@mrcoles.com>
2018-04-18 15:55:21 -0400

    rename test.js to run-test.js

--- javascript/scratch.js
+++ javascript/scratch.js
@@ -1,7 +1,11 @@
-var runTest = require("./test");
+const runTest = require("./run-test");
+
+// START FUNCTION HERE
 
 function f(a, b) {
-  return Object.assign(a, b);
+  return null;
 }
+
+// END FUNCTION
 
 runTest("hard_merge_dicts", f);
